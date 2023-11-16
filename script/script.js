f1f25085deedc65fe8d7e645a9630d4de75d029b
Serg <sergejancev26@gmail.comgit config --listgit config --global user.email sergejancev26@gmail.com>
2023-11-16 18:29:10 +0100

    second commit

--- script/script.js
+++ script/script.js
@@ -1,11 +1,42 @@
-var myArray = ["Пункт 1", "Пункт 2", "Пункт 3"];
+let tasks = ["Пункт 1", "Пункт 2", "Пункт 3"];
 
-var myList = document.createElement('ul');
+function createList() {
+  let ul = document.createElement("ul");
+  ul.id = "taskList"; 
 
-for (var i = 0; i < myArray.length; i++) {
-    var listItem = document.createElement('li');
-    listItem.textContent = myArray[i];
-    myList.appendChild(listItem);
+  tasks.forEach(function(task, index) {
+    let li = document.createElement("li");
+    li.textContent = task;
+
+
+    let cross = document.createElement("span");
+    cross.textContent = "❌";
+    cross.style.color = "red";
+    cross.style.marginLeft = "8px";
+    cross.style.cursor = "pointer";
+    cross.onclick = function() {
+      tasks.splice(index, 1);
+      createList();
+    };
+
+    li.appendChild(cross);
+    ul.appendChild(li);
+  });
+
+  document.body.appendChild(ul);
 }
 
-document.body.appendChild(myList);
+createList();
+
+function addTask() {
+  let newTask = prompt("Введіть нову справу");
+  if (newTask) {
+    tasks.push(newTask);
+    createList();
+  }
+}
+
+let addButton = document.createElement("button");
+addButton.textContent = "Додати";
+addButton.onclick = addTask;
+document.body.appendChild(addButton);
